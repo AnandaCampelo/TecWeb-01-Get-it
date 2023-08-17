e4a79436ed80a5be5600268ffbadb897a63f5b39
AnandaCampelo <anandag.c@hotmail.com>
2023-08-17 08:03:07 -0300

    Handout Parte 3: Separando algumas responsabilidades

--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
+import json
+from pathlib import Path
+
 def extract_route(route):
     return route.replace('HTTP', '/').split(' ')[1][1:]
 
 def read_file(file):
     with open(file, 'r') as f:
         return f.read()
+    
+def load_data(file):
+    data_path = Path('data') / file
+    return json.loads(read_file(data_path))
+
+def load_template(file):
+    template_path = Path('templates') / file
+    return read_file(template_path)
